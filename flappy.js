049e66771bd73732e21bec5b2d8d6267a775a91c
David Baker <davidthbaker409@gmail.com>
2014-11-01 17:20:50 +0000

    Saturday 01.11 17:20

--- flappy.js
+++ flappy.js
@@ -2,6 +2,7 @@
 var stateActions = { preload: preload, create: create, update: update };
 var score = 0;
 var player;
+var pipes;
 
 // Phaser parameters:
 // - game width
@@ -9,7 +10,7 @@ var player;
 // - renderer (go for Phaser.AUTO)
 // - element where the game will be drawn ('game')
 // - actions on the game state (or null for nothing)
-var game = new Phaser.Game(800, 400, Phaser.AUTO, 'game', stateActions);
+var game = new Phaser.Game(700, 400, Phaser.AUTO, 'game', stateActions);
 
 
 /*
@@ -25,6 +26,8 @@ function preload() {
  * Initialises the game. This function is only called once.
  */
 function create() {
+    game.physics.startSystem(Phaser.Physics.ARCADE);
+    pipes = game.add.group();
     game.stage.setBackgroundColor("#CDF3FF");
     score = 0
     //alert(score);
@@ -34,57 +37,71 @@ function create() {
         { font: "25px Arial", //font size and typeface
         fill: "#FF8533" } //text colour
          );
+    //game.input.onDown.add(clickHandler);
 
-    var x = 10;
-    var y = 260;
-    player = game.add.sprite(x, y, "playerImg");
+    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(player_jump);
 
-    game.input.onDown.add(clickHandler);
-
-    game.input.keyboard.addKey(Phaser.Keyboard.SPACEBAR).onDown.add(spaceHandler);
-
-    game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(moveLeft);
-    game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(moveRight);
-    game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(moveUp);
-    game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(moveDown);
+    //game.input.keyboard.addKey(Phaser.Keyboard.LEFT).onDown.add(moveLeft);
+    //game.input.keyboard.addKey(Phaser.Keyboard.RIGHT).onDown.add(moveRight);
+    //game.input.keyboard.addKey(Phaser.Keyboard.UP).onDown.add(moveUp);
+    //game.input.keyboard.addKey(Phaser.Keyboard.DOWN).onDown.add(moveDown);
 
     game.add.audio("score");
 
-    pipeGenerator(150);
-    pipeGenerator(290);
-    pipeGenerator(450);
-    pipeGenerator(650)
+    //pipeGenerator();
+    game.time.events.loop(1.25*Phaser.Timer.SECOND, pipeGenerator)
+
+    var x = 100;
+    var y = 300;
+    player = game.add.sprite(x, y, "playerImg");
+    game.physics.arcade.enable(player);
+    player.anchor.setTo(0.5, 0.5);
+    player.checkWorldBounds = true;
+    player.body.velocity.y = -200;
+    //player.body.velocity.x = 100;
+    player.body.gravity.y = 400;
+
+
 
 }
 
+function add_pipe_part(x, y, pipe_part) {
+    var pipe = pipes.create(x, y, pipe_part);
+    game.physics.arcade.enable(pipe);
+    pipe.body.velocity.x = -400;
 
-function pipeGenerator(x) {
-    var gap = Math.floor(Math.random() * 5) + 1;
+}
 
-    for(var count = 0; count < gap; count++) //for-loop
+function pipeGenerator() {
+    var pipe_offset = 800;
+    var hole = Math.floor(Math.random() * 5) + 1;
+
+    var count;
+
+    for(var count = 0; count < hole; count++) //for-loop
     {
-        game.add.sprite(x, 50 * count, "pipe");
+        add_pipe_part(pipe_offset, 50 * count, "pipe");
     }
 
-    for(var count = gap + 3; count <= 8; count++) //for-loop
+    for(var count = hole + 4; count <= 8; count++) //for-loop
     {
-        game.add.sprite(x, 50 * count, "pipe");
+        add_pipe_part(pipe_offset, 50 * count, "pipe");
     }
 
 }
 
 
-function clickHandler (event) {
-    game.add.sprite(event.x, event.y, "playerImg");
-    game.sound.play("score");
-}
+//function clickHandler (event) {
+    //game.add.sprite(event.x, event.y, "playerImg");
+    //game.sound.play("score");
 
-function spaceHandler() {
+
+//function spaceHandler() {
     //alert("you pressed the spacebar!");
-    score = score + 1;
-    game.sound.play("score");
+    //score = score + 1;
+    //game.sound.play("score");
     //alert(score);
-}
+
 function moveRight() {
     player.x+=20;
 }
@@ -101,9 +118,24 @@ function moveDown() {
     player.y+=20;
 }
 
+function player_jump() {
+    player.body.velocity.y = -200;
+}
+
+function game_over() {
+    //alert("YOU SHALL DIE IN VALHALLA TONIGHT! GAME OVER ");
+    location.reload();
+
+}
+
+
+
+
 // * This function updates the scene. It is called for every new frame.
 
 function update() {
+    game.physics.arcade
+    .overlap(player, pipes, game_over);
 
 }
 
